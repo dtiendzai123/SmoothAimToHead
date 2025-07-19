e876a3886c82364c59b8958aef88518c708b7569
dtiendzai123 <hanknguyen2112@gmail.com>
2025-07-19 22:18:18 +0700

    SmoothAimToHead.js

--- SmoothAimToHead.js
+++ SmoothAimToHead.js
@@ -1,3 +1,7 @@
+const GamePackages = {
+  GamePackage1: "com.dts.freefireth",
+  GamePackage2: "com.dts.freefiremax"
+};
 // ===== Vector3 & KalmanFilter =====
 class Vector3 {
   constructor(x = 0, y = 0, z = 0) { this.x = x; this.y = y; this.z = z; }
